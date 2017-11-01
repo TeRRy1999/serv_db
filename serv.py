5b1ad9c6ee60a227861b206e10fd49a767b922b9
TeRRy <k851032001@gmailc.om>
2017-11-01 12:47:07 +0800

    make it better

--- serv.py
+++ serv.py
@@ -47,5 +47,5 @@ def postpage(pname = "No_name"):
 	return render_template('postform.html',name = pname)
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',debug = True)
+    app.run(host = '0.0.0.0',debug = true)
     
